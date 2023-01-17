1a62eb7044af728e663f4e308398daa745a58f16
Christian Manzaraz <magnusmanz@gmail.com>
2023-01-17 18:43:50 -0300

    Expand and Collapse the Date Picker Cell

--- ToDoList/ToDoDetailTableViewController.swift
+++ ToDoList/ToDoDetailTableViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ToDoDetailTableViewController: UITableViewController {
-
+    
     @IBOutlet var isCompleteButton: UIButton!
     @IBOutlet var titleTextField: UITextField!
     
@@ -19,16 +19,25 @@ class ToDoDetailTableViewController: UITableViewController {
     
     @IBOutlet var saveButton: UIBarButtonItem!
     
+    // To handle “Expand and Collapse the Date Picker Cell"
+    var isDatePickerHidden = true
+    let dateLabelIndexPath = IndexPath(row: 0, section: 1)
+    let datePickerIndexPath = IndexPath(row: 1, section: 1)
+    let notesIndexPath = IndexPath(row: 0, section: 2)
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         dueDateDatePicker.date = Date().addingTimeInterval(24*60*60)
         updateDueDateLabel(date: dueDateDatePicker.date)
         updateSaveButtonState()
         
     }
-
+    
     // MARK: - Helpers
     func updateSaveButtonState() {
         let shouldEnableSaveButton = titleTextField.text?.isEmpty == false
@@ -59,7 +68,36 @@ class ToDoDetailTableViewController: UITableViewController {
     
     
     // MARK: - Table view data source
-
-  
-
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath where isDatePickerHidden == true:
+            return 0
+        case notesIndexPath:
+            return 200
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath:
+            return 216
+        case notesIndexPath:
+            return 200
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath == dateLabelIndexPath {
+            isDatePickerHidden.toggle()
+            updateDueDateLabel(date: dueDateDatePicker.date)
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    }
+    
 }
