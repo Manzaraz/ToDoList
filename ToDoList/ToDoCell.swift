b56cfe8aeef0262eba3588d4a88174ab75685010
Christian Manzaraz <magnusmanz@gmail.com>
2023-01-19 18:50:32 -0300

    Create a Cell Subclass

---
+++ ToDoList/ToDoCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoCell.swift
+//  ToDoList
+//
+//  Created by Christian Manzaraz on 19/01/2023.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoTableViewController.swift
@@ -75,7 +75,7 @@ class ToDoTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier", for: indexPath) as! ToDoCell
         
         // Configure the cell...
         let toDo = toDos[indexPath.row]
