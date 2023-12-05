e951421bee4cee6daf137fcdd936d0091f075714
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-04 23:42:53 -0300

    Register View

---
+++ ToDoList/Views/TLButton.swift
@@ -0,0 +1,18 @@
+//
+//  TLButton.swift
+//  ToDoList
+//
+//  Created by Christian Manzaraz on 04/12/2023.
+//
+
+import SwiftUI
+
+struct TLButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TLButton()
+}
