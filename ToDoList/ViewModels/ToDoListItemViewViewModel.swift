3086d293d355dad03d7d5c17c9f2d4fb1dafa65a
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-13 23:17:42 -0300

    Profile & Log Out

--- ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -6,12 +6,26 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 /// ViewModel for single to do list item view (each rowa in the items list)
 class ToDoListItemViewViewModel: ObservableObject {
     
-    init() {
-        
-    }
+    init() { }
     
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -10,14 +10,65 @@ import SwiftUI
 struct ProfileView: View {
     @StateObject var viewModel = ProfileViewViewModel()
     
-    
     var body: some View {
         NavigationView {
             VStack {
-                
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading Profile...")
+                }
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        // Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        // Info: name, email, member since
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            .padding()
+            
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            
+            HStack {
+                Text("Member since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        .padding()
+        
+        // Sign Out
+        TLButton(title: "Log Out", background: .red) {
+            viewModel.logOut()
+        }
+        .font(.title3)
+        .frame(height: 50)
+        .padding()
+        
+        Spacer()
     }
 }
 
